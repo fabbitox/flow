f1aa8d9938bc1914370a369e99cc23bf40433f84
fabbitox <hyunjin_11@naver.com>
2023-09-14 14:27:56 +0900

    add order by

--- src/main/java/edu/pnu/persistence/AlarmRepository.java
+++ src/main/java/edu/pnu/persistence/AlarmRepository.java
@@ -10,6 +10,7 @@ import edu.pnu.entity.Alarm;
 public interface AlarmRepository extends JpaRepository<Alarm, Integer> {
 	@Query(value = "select name, contact, wl, idcriteria, pred_dt + interval hour hour as wl_dt, alarm_dt "
 			+ "from alarm, member, pred_result, predict "
-			+ "where alarm.idmember = member.idmember and alarm.idpred_result = pred_result.idpred_result and pred_result.idpredict = predict.idpredict", nativeQuery = true)
+			+ "where alarm.idmember = member.idmember and alarm.idpred_result = pred_result.idpred_result and pred_result.idpredict = predict.idpredict "
+			+ "order by alarm_dt desc", nativeQuery = true)
 	public List<Object[]> getLogs();
 }
